2e7b73ad91df5b053c7681290817f83bf17c5b52
cefe!s <fireinspirer@gmail.com>
2023-11-19 13:43:03 +0300

    db migration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import config.db.FlywayConfig;
 import controller.SimpleController;
+import lombok.val;
 
 import java.util.UUID;
 
@@ -7,11 +9,13 @@ public class Main {
     private static final String JSON_USER = "{\"name\":\"Peter\",\"surname\":\"Parker\",\"email\":\"peterparker@gmail.com\",\"age\":25}";
     private static final String JSON_UPDATE_USER = "{\"name\":\"Petr\",\"surname\":\"Park\",\"email\":\"peterp@gmail.com\",\"age\":25}";
     private static final String XML_UPDATE_USER = "<UserDto><name>Petr</name><surname>Parer</surname><email>peter@gmail.com</email><age>24</age></UserDto>";
-    private static final String EXISTING_UUID = "ce6db38a-b275-4a2a-9629-99e4748f85f9";
+    private static final String EXISTING_UUID = "1bbfdbf6-dc0d-4443-9e5d-3c3325d51490";
     private static final UUID ID = UUID.fromString(EXISTING_UUID);
 
     public static void main(String[] args) {
-        SimpleController controller = new SimpleController();
+        val flywayConfig = new FlywayConfig();
+        flywayConfig.flywayTemplate().migrate();
+        val controller = new SimpleController();
         System.out.println(controller.get(ID));
         System.out.println(controller.getAll());
         System.out.println(controller.create(JSON_USER));
